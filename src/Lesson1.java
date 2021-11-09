42b7978e8c143576aca1f0f5be4924c71817fafa
WierdPro <94013950+WierdPro@users.noreply.github.com>
2021-11-09 19:55:42 +0000

    Add files via upload

---
+++ src/Lesson1.java
@@ -0,0 +1,18 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Lesson 1 of java");
+        System.out.println("Again....\n");
+        System.out.println("T\tab!");
+        System.out.println("\"School\"");
+        System.out.println("\\o/");
+        
+        //Comments are muted boi!
+
+        /*
+        Ha
+        Ha
+        Ha
+        Ha
+         */
+    }
+}
---
+++ src/Lesson2.java
@@ -0,0 +1,18 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println("Lesson 2");
+        System.out.println("Variables\n");
+
+        System.out.println("Boolean         1 bit       true or false");
+        System.out.println("Byte            1 byte      -128 to 127");
+        System.out.println("Short           2 bytes     -32,768 to 32,767");
+        System.out.println("Int             4 bytes     -2 billion to 2 billion");
+        System.out.println("Long            8 bytes     -9 quintillion to 9 quintillion\n");
+
+        System.out.println("Float           4 bytes     Fractional number up to 6-7 digits ex. 3.141592f");
+        System.out.println("Double          8 bytes     Fractional number up to 15 digits ex. 3.141592653589793\n");
+
+        System.out.println("Char            2 bytes     Single character/letter/ASCII value ex. 'l'");
+        System.out.println("String          varies      A sequence of characters ex. \"Never gonna give you up!\"");
+    }
+}
